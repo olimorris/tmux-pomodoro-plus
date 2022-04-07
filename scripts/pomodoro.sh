46f113a6b2a7b12d61ec9eb94ec5dfe7488ace77
olimorris <olimorris@users.noreply.github.com>
2022-04-07 15:34:55 +0100

    chore: formatting

--- scripts/pomodoro.sh
+++ scripts/pomodoro.sh
@@ -2,7 +2,7 @@
 
 ################################# SET VARIABLES ################################
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 pomodoro_duration_minutes="@pomodoro_mins"
 pomodoro_break_minutes="@pomodoro_break_mins"
@@ -22,134 +22,123 @@ source $CURRENT_DIR/helpers.sh
 
 ################################# FUNCTIONALITY ################################
 
-get_pomodoro_duration () {
-    get_tmux_option "$pomodoro_duration_minutes" "25"
+get_pomodoro_duration() {
+	get_tmux_option "$pomodoro_duration_minutes" "25"
 }
 
-get_pomodoro_break () {
-    get_tmux_option "$pomodoro_break_minutes" "5"
+get_pomodoro_break() {
+	get_tmux_option "$pomodoro_break_minutes" "5"
 }
 
-get_seconds () {
-    date +%s
+get_seconds() {
+	date +%s
 }
 
-get_notifications () {
-    get_tmux_option "$pomodoro_notifcations" "off"
+get_notifications() {
+	get_tmux_option "$pomodoro_notifcations" "off"
 }
 
-get_sound () {
-    get_tmux_option "$pomodoro_sound" "off"
+get_sound() {
+	get_tmux_option "$pomodoro_sound" "off"
 }
 
-write_to_file () {
-    local data=$1
-    local file=$2
-    echo "$data" > "$file"
+write_to_file() {
+	local data=$1
+	local file=$2
+	echo "$data" >"$file"
 }
 
-read_file () {
-    local file=$1
-    if [ -f $1 ]
-    then
-        cat $1
-    else
-        echo -1
-    fi
+read_file() {
+	local file=$1
+	if [ -f $1 ]; then
+		cat $1
+	else
+		echo -1
+	fi
 }
 
-remove_file () {
-    local file=$1
-    if [ -f $file ]
-    then
-        rm $file
-    fi
+remove_file() {
+	local file=$1
+	if [ -f $file ]; then
+		rm $file
+	fi
 }
 
-if_inside_tmux () {
-    test -n "${TMUX}"
+if_inside_tmux() {
+	test -n "${TMUX}"
 }
 
-send_notification () {
-    if [ $(get_notifications) == 'on' ]
-    then
-        local title=$1
-        local message=$2
-        local sound=$(get_sound)
-        if [ "$sound" == "off" ]
-        then
-            osascript -e 'display notification "'"$message"'" with title "'"$title"'"'
-        else
-            osascript -e 'display notification "'"$message"'" with title "'"$title"'" sound name "'"$sound"'"'
-        fi
-    fi
+send_notification() {
+	if [ $(get_notifications) == 'on' ]; then
+		local title=$1
+		local message=$2
+		local sound=$(get_sound)
+		if [ "$sound" == "off" ]; then
+			osascript -e 'display notification "'"$message"'" with title "'"$title"'"'
+		else
+			osascript -e 'display notification "'"$message"'" with title "'"$title"'" sound name "'"$sound"'"'
+		fi
+	fi
 }
 
-clean_env () {
-    remove_file "$POMODORO_FILE"
-    remove_file "$POMODORO_STATUS_FILE"
+clean_env() {
+	remove_file "$POMODORO_FILE"
+	remove_file "$POMODORO_STATUS_FILE"
 }
 
-pomodoro_start () {
-    clean_env
-    mkdir -p $POMODORO_DIR
-    write_to_file $(get_seconds) $POMODORO_FILE
-    send_notification "🍅 Pomodoro started!" "Your Pomodoro is underway"
-    if_inside_tmux && tmux refresh-client -S
-    return 0
+pomodoro_start() {
+	clean_env
+	mkdir -p $POMODORO_DIR
+	write_to_file $(get_seconds) $POMODORO_FILE
+	send_notification "🍅 Pomodoro started!" "Your Pomodoro is underway"
+	if_inside_tmux && tmux refresh-client -S
+	return 0
 }
 
-pomodoro_cancel () {
-    clean_env
-    send_notification "🍅 Pomodoro cancelled!" "Your Pomodoro was cancelled"
-    if_inside_tmux && tmux refresh-client -S
-    return 0
+pomodoro_cancel() {
+	clean_env
+	send_notification "🍅 Pomodoro cancelled!" "Your Pomodoro was cancelled"
+	if_inside_tmux && tmux refresh-client -S
+	return 0
 }
 
-pomodoro_status () {
-    local pomodoro_start_time=$(read_file "$POMODORO_FILE")
-    local pomodoro_status=$(read_file "$POMODORO_STATUS_FILE")
-    local current_time=$(get_seconds)
-    local difference=$(( ($current_time - $pomodoro_start_time) / 60 ))
-    
-    if [ $pomodoro_start_time -eq -1 ]
-    then
-        echo ""
-    elif [ $difference -ge $(( $(get_pomodoro_duration) + $(get_pomodoro_break) )) ]
-    then
-        pomodoro_start_time=-1
-        echo ""
-        if [ $pomodoro_status == 'on_break' ]
-        then
-            send_notification "🍅 Break finished!" "Your Pomodoro break is now over"
-            write_to_file "break_complete" "$POMODORO_STATUS_FILE"
-        fi
-    elif [ $difference -ge $(get_pomodoro_duration) ]
-    then
-        if [ $pomodoro_status -eq -1 ]
-        then
-            send_notification "🍅 Pomodoro completed!" "Your Pomodoro has now completed"
-            write_to_file "on_break" "$POMODORO_STATUS_FILE"
-        fi
-        printf "$(get_tmux_option "$pomodoro_complete" "$pomodoro_complete_default")$(( -($difference - $(get_pomodoro_duration) - $(get_pomodoro_break)) )) "
-    else
-        printf "$(get_tmux_option "$pomodoro_on" "$pomodoro_on_default")$(( $(get_pomodoro_duration) - $difference )) "
-    fi
+pomodoro_status() {
+	local pomodoro_start_time=$(read_file "$POMODORO_FILE")
+	local pomodoro_status=$(read_file "$POMODORO_STATUS_FILE")
+	local current_time=$(get_seconds)
+	local difference=$((($current_time - $pomodoro_start_time) / 60))
+
+	if [ $pomodoro_start_time -eq -1 ]; then
+		echo ""
+	elif [ $difference -ge $(($(get_pomodoro_duration) + $(get_pomodoro_break))) ]; then
+		pomodoro_start_time=-1
+		echo ""
+		if [ $pomodoro_status == 'on_break' ]; then
+			send_notification "🍅 Break finished!" "Your Pomodoro break is now over"
+			write_to_file "break_complete" "$POMODORO_STATUS_FILE"
+		fi
+	elif [ $difference -ge $(get_pomodoro_duration) ]; then
+		if [ $pomodoro_status -eq -1 ]; then
+			send_notification "🍅 Pomodoro completed!" "Your Pomodoro has now completed"
+			write_to_file "on_break" "$POMODORO_STATUS_FILE"
+		fi
+		printf "$(get_tmux_option "$pomodoro_complete" "$pomodoro_complete_default")$((-($difference - $(get_pomodoro_duration) - $(get_pomodoro_break)))) "
+	else
+		printf "$(get_tmux_option "$pomodoro_on" "$pomodoro_on_default")$(($(get_pomodoro_duration) - $difference)) "
+	fi
 }
 
-main () {
-    cmd=$1
-    shift
-    
-    if [ "$cmd" = "start" ]
-    then
-        pomodoro_start
-    elif [ "$cmd" = "cancel" ]
-    then
-        pomodoro_cancel
-    else
-        pomodoro_status
-    fi
+main() {
+	cmd=$1
+	shift
+
+	if [ "$cmd" = "start" ]; then
+		pomodoro_start
+	elif [ "$cmd" = "cancel" ]; then
+		pomodoro_cancel
+	else
+		pomodoro_status
+	fi
 }
 
 main $@
