672c8cd7a18d11b914a76e866d55524c54393575
basaran <30809170+basaran@users.noreply.github.com>
2022-07-14 14:33:59 -0400

    feat: add linux notification and manual timer
    * added linux notification
    added "m" to string
    
    * added manual timer prompt

--- scripts/pomodoro.sh
+++ scripts/pomodoro.sh
@@ -73,11 +73,18 @@ send_notification() {
 		local title=$1
 		local message=$2
 		local sound=$(get_sound)
-		if [ "$sound" == "off" ]; then
-			osascript -e 'display notification "'"$message"'" with title "'"$title"'"'
-		else
-			osascript -e 'display notification "'"$message"'" with title "'"$title"'" sound name "'"$sound"'"'
+
+		if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+			notify-send -t 5000 "$title" "$message"
+
+		elif [[ "$OSTYPE" == "darwin"* ]]; then
+			if [[ sound == "on" ]]; then
+				osascript -e 'display notification "'"$message"'" with title "'"$title"'" sound name "'"$sound"'"'
+			else
+				osascript -e 'display notification "'"$message"'" with title "'"$title"'"'
+			fi
 		fi
+
 	fi
 }
 
@@ -102,6 +109,10 @@ pomodoro_cancel() {
 	return 0
 }
 
+pomodoro_manual() {
+	tmux command-prompt -p 'Pomodoro Mins:' 'set -g @pomodoro_mins %1'
+}
+
 pomodoro_status() {
 	local pomodoro_start_time=$(read_file "$POMODORO_FILE")
 	local pomodoro_status=$(read_file "$POMODORO_STATUS_FILE")
@@ -122,9 +133,9 @@ pomodoro_status() {
 			send_notification "🍅 Pomodoro completed!" "Your Pomodoro has now completed"
 			write_to_file "on_break" "$POMODORO_STATUS_FILE"
 		fi
-		printf "$(get_tmux_option "$pomodoro_complete" "$pomodoro_complete_default")$((-($difference - $(get_pomodoro_duration) - $(get_pomodoro_break)))) "
+		printf "$(get_tmux_option "$pomodoro_complete" "$pomodoro_complete_default")$((-($difference - $(get_pomodoro_duration) - $(get_pomodoro_break))))m "
 	else
-		printf "$(get_tmux_option "$pomodoro_on" "$pomodoro_on_default")$(($(get_pomodoro_duration) - $difference)) "
+		printf "$(get_tmux_option "$pomodoro_on" "$pomodoro_on_default")$(($(get_pomodoro_duration) - $difference))m "
 	fi
 }
 
@@ -136,6 +147,8 @@ main() {
 		pomodoro_start
 	elif [ "$cmd" = "cancel" ]; then
 		pomodoro_cancel
+	elif [ "$cmd" = "manual" ]; then
+		pomodoro_manual
 	else
 		pomodoro_status
 	fi
