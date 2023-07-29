e604c151485a9d47fdd230cbcbfe3a19089f79f3
matteodiga <matteo.diga95@gmail.com>
2023-07-29 14:57:10 +0200

    feat: add auto_start_break feature
    Add auto_start_break feature that prevent break time to start automatically
    so, after the pomodoro focus time end, user has to manually start the break.
    This way user can choose when start the break time based on what it's doing.
    
    The flow is:
    
    - start pomodoro focus time     - user action (<tmux-prefix> p)
    - focus time...
    - end focus time, ask for a break
    - wait for a break...
    - start pomodoro break time     - user action(<tmux-prefix> p)
    - break time...
    - end pomodoro
    
    The auto_start_break feature is disable by default.

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -38,3 +38,15 @@ write_to_file() {
 	local file=$2
 	echo "$data" >"$file"
 }
+
+debug_log () {
+    # add log print into the code (debug_log "hello from tmux_pomodoro_plus)"
+    # set true to enable log messages
+    # follow the log using "tail -f /tmp/tmux_pomodoro_debug_log/log.txt"
+    if true ; then
+        DIR="/tmp/tmux_pomodoro_debug_log/"
+        FILE="log.txt"
+        mkdir -p $DIR
+        echo "$(date +%T) " "$1" >> "$DIR/$FILE"
+    fi
+}
